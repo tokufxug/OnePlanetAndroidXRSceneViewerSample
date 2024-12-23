c0ea05b4d79c50dc491d070860dbfbe39cbe87a3
tokufxug <tokufxug@gmail.com>
2024-12-24 00:01:03 +0900

    Initial commit

---
+++ app/src/main/java/jp/co/oneplanet/androidxr/sample/oneplanetandroidxrsceneviewersample/MainActivity.kt
@@ -0,0 +1,182 @@
+package jp.co.oneplanet.androidxr.sample.oneplanetandroidxrsceneviewersample
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.xr.compose.platform.LocalHasXrSpatialFeature
+import androidx.xr.compose.platform.LocalSession
+import androidx.xr.compose.platform.LocalSpatialCapabilities
+import androidx.xr.compose.spatial.EdgeOffset
+import androidx.xr.compose.spatial.Orbiter
+import androidx.xr.compose.spatial.OrbiterEdge
+import androidx.xr.compose.spatial.Subspace
+import androidx.xr.compose.subspace.SpatialPanel
+import androidx.xr.compose.subspace.layout.SpatialRoundedCornerShape
+import androidx.xr.compose.subspace.layout.SubspaceModifier
+import androidx.xr.compose.subspace.layout.height
+import androidx.xr.compose.subspace.layout.movable
+import androidx.xr.compose.subspace.layout.resizable
+import androidx.xr.compose.subspace.layout.width
+import jp.co.oneplanet.androidxr.sample.oneplanetandroidxrsceneviewersample.ui.theme.OnePlanetAndroidXRSceneViewerSampleTheme
+
+class MainActivity : ComponentActivity() {
+
+    @SuppressLint("RestrictedApi")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            OnePlanetAndroidXRSceneViewerSampleTheme {
+                val session = LocalSession.current
+                if (LocalSpatialCapabilities.current.isSpatialUiEnabled) {
+                    Subspace {
+                        MySpatialContent(onRequestHomeSpaceMode = { session?.requestHomeSpaceMode() })
+                    }
+                } else {
+                    My2DContent(onRequestFullSpaceMode = { session?.requestFullSpaceMode() })
+                }
+            }
+        }
+    }
+
+    fun launchSceneViewer() {
+        val THREED_MODEL_URL = "https://modelviewer.dev/shared-assets/models/RobotExpressive.glb"
+        val MIME_TYPE = "model/gltf-binary"
+        val sceneViewerIntent = Intent(Intent.ACTION_VIEW)
+        val intentUri = Uri.parse("https://arvr.google.com/scene-viewer/1.2")
+            .buildUpon()
+            .appendQueryParameter("file", THREED_MODEL_URL)
+            .build()
+        sceneViewerIntent.setDataAndType(intentUri, MIME_TYPE)
+        startActivity(sceneViewerIntent)
+    }
+}
+
+@SuppressLint("RestrictedApi")
+@Composable
+fun MySpatialContent(onRequestHomeSpaceMode: () -> Unit) {
+    SpatialPanel(SubspaceModifier.width(1280.dp).height(800.dp).resizable().movable()) {
+        Surface {
+            MainContent(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(48.dp)
+            )
+        }
+    }
+}
+
+@SuppressLint("RestrictedApi")
+@Composable
+fun My2DContent(onRequestFullSpaceMode: () -> Unit) {
+    Surface {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            MainContent(modifier = Modifier.padding(48.dp))
+            if (LocalHasXrSpatialFeature.current) {
+                FullSpaceModeIconButton(
+                    onClick = onRequestFullSpaceMode,
+                    modifier = Modifier.padding(32.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MainContent(modifier: Modifier = Modifier) {
+    Box(
+        Modifier
+            .background(color = Color.LightGray)
+            .height(1280.dp)
+            .width(800.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        SceneViewerButton()
+    }
+}
+
+@Composable
+fun SceneViewerButton() {
+    val context = LocalContext.current
+    Button(onClick = {
+        (context as? MainActivity)?.launchSceneViewer()
+    }) {
+        Text(text = "3Dモデル表示")
+    }
+}
+
+@Composable
+fun FullSpaceModeIconButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    IconButton(onClick = onClick, modifier = modifier) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_full_space_mode_switch),
+            contentDescription = stringResource(R.string.switch_to_full_space_mode)
+        )
+    }
+}
+
+@Composable
+fun HomeSpaceModeIconButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    FilledTonalIconButton(onClick = onClick, modifier = modifier) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_home_space_mode_switch),
+            contentDescription = stringResource(R.string.switch_to_home_space_mode)
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun My2dContentPreview() {
+    OnePlanetAndroidXRSceneViewerSampleTheme {
+        My2DContent(onRequestFullSpaceMode = {})
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FullSpaceModeButtonPreview() {
+    OnePlanetAndroidXRSceneViewerSampleTheme {
+        FullSpaceModeIconButton(onClick = {})
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun HomeSpaceModeButtonPreview() {
+    OnePlanetAndroidXRSceneViewerSampleTheme {
+        HomeSpaceModeIconButton(onClick = {})
+    }
+}
